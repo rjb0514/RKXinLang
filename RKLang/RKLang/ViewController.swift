ff2937c70138063770015a15a1749eb552445479
ru <ru@.com>
2016-11-13 21:56:36 +0800

    creat project

---
+++ RKLang/RKLang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RKLang
+//
+//  Created by ru on 16/11/13.
+//  Copyright © 2016年 ru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
